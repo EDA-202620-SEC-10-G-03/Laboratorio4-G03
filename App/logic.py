15b9c46c0bd49fd85e5fece3adf84ca018a45e29
Alvaro Villate Leon <a.villatel@uniandes.edu.co>
2026-08-27 10:30:24 -0500

    Agregados

--- App/logic.py
+++ App/logic.py
@@ -27,7 +27,10 @@
 import csv
 import os
 import time
+
+from DataStructures.Stack import stack as st
 from DataStructures.List import array_list as lt
+from DataStructures.List import single_linked_list as sll
 # TODO Importar las librerías correspondientes para el manejo de pilas y colas
 
 data_dir = os.path.dirname(os.path.realpath('__file__')) + '/Data/'
